318a0596b44d2de3fd26d90da478e686661f2b07
BAEK MIJIN <128350820+mijinB@users.noreply.github.com>
2024-06-02 08:45:54 +0000

    web scraper(weworkremotely) + pagination

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
+all_jobs = []
 
-response = requests.get(url)
+def scrape_page(url):
+  response = requests.get(url)
+  soup = BeautifulSoup(response.content, "html.parser")
+  jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
 
-soup = BeautifulSoup(response.content, "html.parser")
+  for job in jobs:
+    title = job.find("span", class_="title").text
+    company, position, region = job.find_all("span", class_="company")[0:3]
+    try:
+      find_url = job.find("div", class_="tooltip--flag-logo").next_sibling["href"]
+      url = f"https://weworkremotely.com{find_url}"
+    except KeyError:
+      url = "You need log-in"
+    job_data = {
+      "title": title,
+      "company": company.text,
+      "position": position.text,
+      "region": region.text,
+      "url": url
+    }
+    all_jobs.append(job_data)
 
-#"find_all" error 때문에 주석하고 git push
-# jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
+def get_pages(url):
+  response = requests.get(url)
+  soup = BeautifulSoup(response.content, "html.parser")
+  return len(soup.find("div", class_="pagination").find_all("span", class_="page"))
 
-# for job in jobs:
-#   title = job.find("span", class_="title").text
-  
-#   company, position, region = job.find_all("span", class_="company")[0:3]
-#   company = company.text
-#   position = position.text
-#   region = region.text
-#   print(title, company, position, region, "-----\n")
+total_pages = get_pages("https://weworkremotely.com/remote-full-time-jobs?page=1")
+
+for x in range(total_pages):
+  url = f"https://weworkremotely.com/remote-full-time-jobs?page={x+1}"
+  scrape_page(url)
+
+print(len(all_jobs))
